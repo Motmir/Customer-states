db9d8f6bcf806b6884fad209e9c8bd5b29e1496e
Eirik Akerhaugen <Eirik_akerhaugen@hotmail.com>
2024-07-27 01:12:14 +0200

    Some test level thingy

--- Assets/Scripts/Punch_minigame.cs
+++ Assets/Scripts/Punch_minigame.cs
@@ -24,12 +24,12 @@ public class Punch_minigame : MonoBehaviour
     // Update is called once per frame
     void FixedUpdate()
     {
-        
         if (progressBar.GetComponent<Image>().fillAmount < 1)
         {
             if (punchAction.triggered != false)
             {
                 progress = 0.05f;
+                ApplyForce();
             } else
             {
                 progress = -0.003f;
@@ -45,6 +45,19 @@ public class Punch_minigame : MonoBehaviour
         {
             progressBar.GetComponent<Image>().color = Color.Lerp(Color.red, Color.green, progressBar.GetComponent<Image>().fillAmount);
             yield return null;
+        }
+    }
+
+    private void ApplyForce()
+    {
+        Rigidbody2D torso = GameObject.Find("torso").GetComponent<Rigidbody2D>();
+        int option = Random.Range(1, 3);
+        if (option == 1)
+        {
+            torso.velocity = Vector2.left * 5;
+        } else if (option == 2)
+        {
+            torso.velocity = Vector2.right * 5;
         }
     }
 }
