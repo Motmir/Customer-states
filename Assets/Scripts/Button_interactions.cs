e36c50de6c4db5416df0b6a042125eed63d43316
Eirik Akerhaugen <Eirik_akerhaugen@hotmail.com>
2024-07-26 19:08:11 +0200

    Added send_out and discard
    Added two buttons that can do the desired send out and discard functionality

---
+++ Assets/Scripts/Button_interactions.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Button_interactions : MonoBehaviour
+{
+    //public GameObject current_robot;
+    public void Send_out()
+    {
+        GameObject current_robot = GameObject.Find("Robot");
+        Rigidbody2D robot_rb = current_robot.GetComponent<Rigidbody2D>();
+        robot_rb.velocity = Vector3.left;
+        Destroy(current_robot, 5f);
+    }
+
+    public void Discard()
+    {
+        GameObject current_robot = GameObject.Find("Robot");
+        Rigidbody2D robot_rb = current_robot.GetComponent<Rigidbody2D>();
+        robot_rb.velocity = Vector3.down;
+        Destroy(current_robot, 2f);
+    }
+}
