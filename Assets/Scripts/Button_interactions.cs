655fed6955b0cf98a8b9ac228f7c357ef1072048
Eirik Akerhaugen <Eirik_akerhaugen@hotmail.com>
2024-07-26 20:08:02 +0200

    Mover buttons

--- Assets/Scripts/Button_interactions.cs
+++ Assets/Scripts/Button_interactions.cs
@@ -9,7 +9,7 @@ public class Button_interactions : MonoBehaviour
     {
         GameObject current_robot = GameObject.Find("Robot");
         Rigidbody2D robot_rb = current_robot.GetComponent<Rigidbody2D>();
-        robot_rb.velocity = Vector3.left;
+        robot_rb.velocity = Vector3.right;
         Destroy(current_robot, 5f);
     }
 
