96e416ca424174745a1159cbbb10b4edc565f334
Eirik Akerhaugen <Eirik_akerhaugen@hotmail.com>
2024-07-26 23:38:16 +0200

    Robot template added

--- Assets/Scripts/Punch_minigame.cs
+++ Assets/Scripts/Punch_minigame.cs
@@ -2,29 +2,49 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
+using UnityEngine.UI;
 
 public class Punch_minigame : MonoBehaviour
 {
     [SerializeField] private InputAction punchAction;
+    private GameObject progressBar;
+    private float progress;
 
     private void OnEnable()
     {
         punchAction.Enable();
+        progress = 0;
+        progressBar = GameObject.Find("Bar");
+        StartCoroutine(ChangeBarColor());
     }
-
     private void OnDisable()
     {
         punchAction.Disable();
     }
-
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
-        GameObject progressBar = GameObject.Find("Bar");
-        Debug.Log(punchAction.triggered);
-        if (punchAction.triggered != false)
+        
+        if (progressBar.GetComponent<Image>().fillAmount < 1)
         {
-            progressBar.GetComponent("Image")
+            if (punchAction.triggered != false)
+            {
+                progress = 0.05f;
+            } else
+            {
+                progress = -0.003f;
+            }
+            
+            progressBar.GetComponent<Image>().fillAmount += progress;
+        }
+    }
+
+    private IEnumerator ChangeBarColor()
+    {
+        while (progressBar.GetComponent<Image>().fillAmount < 1)
+        {
+            progressBar.GetComponent<Image>().color = Color.Lerp(Color.red, Color.green, progressBar.GetComponent<Image>().fillAmount);
+            yield return null;
         }
     }
 }
