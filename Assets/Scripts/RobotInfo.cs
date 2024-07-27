754ce781f08548582bf70acc7e2c798b524b6aa2
AuburnGhost <auburnghost.tx@gmail.com>
2024-07-27 18:57:12 +0200

    created robot sets

---
+++ Assets/Scripts/RobotInfo.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu]
+public class RobotSet : ScriptableObject
+{
+    public Sprite torsoOff;
+    public Sprite torsoOn;
+    public Sprite headOff;
+    public Sprite headOn;
+    public Sprite headActive;
+    public Sprite Arm;
+    public Sprite Leg;
+
+
+}
