b78ac1e27bc73317a8d1e13b9d92f89f1d61c102
AuburnGhost <auburnghost.tx@gmail.com>
2024-07-27 16:59:55 +0200

    robot script  edit

--- Assets/Scripts/Robot.cs
+++ Assets/Scripts/Robot.cs
@@ -5,22 +5,28 @@ using UnityEngine;
 public class Robot : MonoBehaviour
 {
     private LevelControl manager;
+    private GameObject head;
     private GameObject torso;
+    private GameObject leftLeg;
+    private GameObject rightLeg;
+    private GameObject leftArm;
+    private GameObject rightArm;
 
     // Start is called before the first frame update
     void Awake()
     {
         manager = GameObject.Find("LevelManager").GetComponent<LevelControl>();
         torso = this.gameObject.transform.Find("torso").gameObject;
+        head = this.gameObject.transform.Find("head").gameObject;
+        leftLeg = this.gameObject.transform.Find("leftLeg").gameObject;
+        rightLeg = this.gameObject.transform.Find("rightLeg").gameObject;
+        leftArm = this.gameObject.transform.Find("leftArm").gameObject;
+        rightArm = this.gameObject.transform.Find("rightArm").gameObject;
+
         torso.GetComponent<HingeJoint2D>().connectedBody = manager.conveyorClaw.GetComponent<Rigidbody2D>();
     }
 
-    public void Screwdriver(Collider2D part){
-        Debug.Log(part.name);
-    }
-    public void Blowtorch(Collider2D part){
-        Debug.Log(part.name);
-    }
+
 
     // Update is called once per frame
     public void Discard()
