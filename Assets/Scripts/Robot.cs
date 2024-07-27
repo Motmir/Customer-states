dcae9ed005ba240a7622dfe1b410b278169f683a
Eirik Akerhaugen <Eirik_akerhaugen@hotmail.com>
2024-07-27 18:21:04 +0200

    Roomba executed

--- Assets/Scripts/Robot.cs
+++ Assets/Scripts/Robot.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class Robot : MonoBehaviour
@@ -17,13 +18,35 @@ public class Robot : MonoBehaviour
     {
         manager = GameObject.Find("LevelManager").GetComponent<LevelControl>();
         torso = this.gameObject.transform.Find("torso").gameObject;
-        head = this.gameObject.transform.Find("head").gameObject;
-        leftLeg = this.gameObject.transform.Find("leftLeg").gameObject;
-        rightLeg = this.gameObject.transform.Find("rightLeg").gameObject;
-        leftArm = this.gameObject.transform.Find("leftArm").gameObject;
-        rightArm = this.gameObject.transform.Find("rightArm").gameObject;
+        Transform headTransform = this.gameObject.transform.Find("head");
+        if (headTransform != null)
+        {
+            head = headTransform.gameObject;
+        }
+        Transform leftLegTransform = this.gameObject.transform.Find("leftLeg");
+        if (leftLegTransform != null)
+        {
+            leftLeg = leftLegTransform.gameObject;
+        }
+        Transform rightLegTransform = this.gameObject.transform.Find("rightLeg");
+        if (rightLegTransform != null)
+        {
+            rightLeg = rightLegTransform.gameObject;
+        }
+        Transform leftArmTransform = this.gameObject.transform.Find("leftArm");
+        if (leftArmTransform != null)
+        {
+            leftArm = leftArmTransform.gameObject;
+        }
+        Transform rightArmTransform = this.gameObject.transform.Find("rightArm");
+        if (rightArmTransform != null)
+        {
+            rightArm = rightArmTransform.gameObject;
+        }
 
         torso.GetComponent<HingeJoint2D>().connectedBody = manager.conveyorClaw.GetComponent<Rigidbody2D>();
+        torso.GetComponent<HingeJoint2D>().connectedAnchor = new Vector2(0, -0.6f);
+
     }
 
 
