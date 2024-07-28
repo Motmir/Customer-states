820bb5e29905781a1886b3b137763434b6a411d4
Eirik Akerhaugen <Eirik_akerhaugen@hotmail.com>
2024-07-28 12:07:07 +0200

    Added more buttons and potential dialogue system

---
+++ Assets/Scripts/DialogueManager.cs
@@ -0,0 +1,50 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class DialogueManager : MonoBehaviour
+{
+    private Queue<string> sentences;
+    public TextMeshProUGUI nameText;
+    public TextMeshProUGUI dialogueText;
+    public GameObject dialogueBox;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        sentences = new Queue<string>();
+    }
+
+    public void StartDialogue(Dialogue dialogue)
+    {
+        dialogueBox.SetActive(true);
+        nameText.text = dialogue.name;
+
+        sentences.Clear();
+
+        foreach (string sentence in dialogue.sentences)
+        {
+            sentences.Enqueue(sentence);
+        }
+
+        DisplayNextSentence();
+    }
+
+    public void DisplayNextSentence()
+    {
+        if (sentences.Count == 0) 
+        {
+            EndDialogue();
+            return;
+        }
+
+        string sentence = sentences.Dequeue();
+        dialogueText.text = sentence;
+    }
+
+    public void EndDialogue()
+    {
+        dialogueBox.SetActive(false);
+    }
+}
