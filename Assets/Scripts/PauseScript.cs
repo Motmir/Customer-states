e7c3e95a4736db9a779426cd2ebac5879dadd192
Nessie42 <mathea1708@gmail.com>
2024-07-28 15:01:32 +0200

    pause menu

--- Assets/Scripts/PauseScript.cs
+++ Assets/Scripts/PauseScript.cs
@@ -2,10 +2,20 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class PauseScript : MonoBehaviour
 {
     [SerializeField] private GameObject pauseMenu;
+    [SerializeField] private GameObject bookBackground;
+    [SerializeField] private GameObject menuB;
+    [SerializeField] private GameObject settingsB;
+    [SerializeField] private GameObject helpB;
+    [SerializeField] private GameObject backB;
+    [SerializeField] private GameObject team;
+    [SerializeField] private GameObject helpText;
+
+
 
     public void pauseButton()
     {
@@ -21,5 +31,43 @@ public class PauseScript : MonoBehaviour
             pauseMenu.SetActive(false);
         }
 
+    }
+
+    public void menuButton()
+    {
+        SceneManager.LoadScene(0);
+    }
+
+    public void settingsButton()
+    {
+        SceneManager.LoadScene(4);
+    }
+
+    public void backButton()
+    {
+        Time.timeScale = 1;
+        pauseMenu.SetActive(false);
+    }
+
+    public void helpButton()
+    {
+        menuB.SetActive(false);
+        settingsB.SetActive(false);
+        backB.SetActive(false);
+        helpB.SetActive(false);
+        team.SetActive(false);
+        helpText.SetActive(true);
+
+
+    }
+
+    public void helpBack()
+    {
+        menuB.SetActive(true);
+        settingsB.SetActive(true);
+        backB.SetActive(true);
+        helpB.SetActive(true);
+        team.SetActive(true);
+        helpText.SetActive(false);
     }
 }
