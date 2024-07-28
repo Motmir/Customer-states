4c127ece98de8320166ff49c1e05f83b8d78acaa
Eirik Akerhaugen <Eirik_akerhaugen@hotmail.com>
2024-07-28 10:37:49 +0200

    Fetch new when ran around

--- Assets/Scripts/LevelManager.cs
+++ Assets/Scripts/LevelManager.cs
@@ -1,6 +1,8 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using Unity.Mathematics;
+using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
@@ -16,6 +18,7 @@ public class LevelControl : MonoBehaviour
     [NonSerialized] public bool done;
     [SerializeField] private GameObject userInterface;
     public int scene;
+    private GameObject crane;
 
     // Start is called before the first frame update
     public void Start()
@@ -27,13 +30,29 @@ public class LevelControl : MonoBehaviour
         float yPos = Camera.main.ScreenToWorldPoint(conveyorArm.transform.position).y;
         robot = Instantiate(this.robotPrefab, new Vector3(xPos, yPos, 0), Quaternion.identity);
         roboScript = robot.GetComponent<Robot>();
+        crane = GameObject.Find("Crane");
+    }
+
+    public void FetchNextRobot()
+    {
+        float xPos = Camera.main.ScreenToWorldPoint(conveyorArm.transform.position).x;
+        float yPos = Camera.main.ScreenToWorldPoint(conveyorArm.transform.position).y;
+        robot = Instantiate(this.robotPrefab, new Vector3(xPos, yPos, 0), Quaternion.identity);
+        DisableRobotCollision();
+        Invoke("EnableRobotCollision", 2f);
     }
 
     // Update is called once per frame
     void Update()
     {
+                
+        if (conveyorArm.transform.position.y != 3.88f)
+        {
+            crane.transform.position = new Vector3(0, 0, 0);
+            conveyorArm.transform.position = new Vector3(conveyorArm.transform.position.x, 3.88f, conveyorArm.transform.position.z);
+        }
         if (conveyorArm.transform.position.x <= 0 || 
-            (done == true && conveyorArm.transform.position.x <= 500))
+            (done == true && conveyorArm.transform.position.x <= 15))
         {
             userInterface.SetActive(false);
             conveyorArm.GetComponent<Rigidbody2D>().velocity = Vector2.right * 2;
@@ -42,7 +61,32 @@ public class LevelControl : MonoBehaviour
         {
             userInterface.SetActive(true);
             conveyorArm.GetComponent<Rigidbody2D>().velocity = Vector2.zero;
+            if (conveyorArm.transform.position.x >= 15)
+            {
+                Destroy(robot);
+                crane.SetActive(false);
+                conveyorArm.transform.position = new Vector3(-25, 3.88f, -0.1f);
+                crane.SetActive(true);
+                Invoke("FetchNextRobot", 0.4f);
+                done = false;
+            }
         }
+    }
+
+    public void DisableRobotCollision()
+    {
+        //Disable torso
+        robot.GetComponentInChildren<BoxCollider2D>().enabled = false;
+        //Disable the rest
+        robot.GetComponentInChildren<CapsuleCollider2D>().enabled = false;
+    }
+
+    public void EnableRobotCollision()
+    {
+        //Enable torso
+        robot.GetComponentInChildren<BoxCollider2D>().enabled = true;
+        //Enable the rest
+        robot.GetComponentInChildren<CapsuleCollider2D>().enabled = true;
     }
 
     public void setDone()
